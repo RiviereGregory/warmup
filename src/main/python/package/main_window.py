a386c1ea4fc4ea3f11f943cc0bbb08dc302eb0d7
RiviereGregory <gregory.riviere@laposte.net>
2020-12-06 13:29:00 +0100

    Ajout Fenêtre principale

---
+++ src/main/python/package/main_window.py
@@ -0,0 +1,7 @@
+from PySide2 import QtWidgets
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Application d'exemple")
+
